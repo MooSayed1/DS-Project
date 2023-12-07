5b53c2d92ce8f5df385b2b7941a982b892fcd198
AnonMiraj <nabilmalek48@gmail.com>
2023-12-07 23:17:24 +0000

    added small gui interface

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
-#include <iostream>
-#include "Cake.h"
-#include "Subll.h"
-#include "Stack.h"
-using namespace std;
-
+#include <raylib.h>
+#include "menu/menu.h"
 int main() {
-  Subll<Stack<string>>s;
-  
-  // s.push();
-  // s.push();
-  // s.push();
-  // s.push();
-  // s.push();
-  // s.push();
-  // s.push();
-  // s.push();
-  // s.print();
-  return 0;
+
+    size_t screenWidth = 800;
+    size_t screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Cake Factory");
+
+    Menu obj(screenWidth,screenHeight);
+    obj.menu_head("Comfy Purble Factory");
+    obj.add("Order a Cake", ORDER_CAKE);
+    obj.add("Factory Stimulation", FACTORY_SIMULATION);
+    obj.add("Exit", EXIT);
+
+    int choice = -1;
+    while (choice != EXIT) {
+        choice = obj.display();
+
+        switch (choice) {
+            case ORDER_CAKE:
+                // Handle order a cake
+                break;
+            case FACTORY_SIMULATION:
+                // Handle factory stimulation
+                break;
+        }
+    }
+
+    CloseWindow();
+    return 0;
 }
+
