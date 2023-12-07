c873142455c468e127e8783eac84a0819b2c6e53
AnonMiraj <nabilmalek48@gmail.com>
2023-12-07 00:25:10 +0000

    added subll base class

--- Queue.h
+++ Queue.h
@@ -2,51 +2,73 @@
 #define QUEUE_H
 #include <iostream>
 
-template <typename T, size_t Capacity> class Queue {
+template <typename T, size_t InitialCapacity = 10>
+class Queue {
 private:
-  T elements[Capacity];
-  size_t front;
-  size_t rear;
-  size_t size;
+    T *elements;
+    size_t capacity;
+    size_t front;
+    size_t rear;
+    size_t size;
+
+    void resize() {
+        size_t newCapacity = capacity * 2; 
+        T *newElements = new T[newCapacity];
+
+        for (size_t i = 0; i < size; ++i) {
+            newElements[i] = elements[(front + i) % capacity];
+        }
+
+        delete[] elements;
+
+        elements = newElements;
+        capacity = newCapacity;
+        front = 0;
+        rear = size;
+    }
 
 public:
-  Queue() : front(0), rear(0), size(0) {}
+    Queue() : elements(new T[InitialCapacity]), capacity(InitialCapacity), front(0), rear(0), size(0) {}
 
-  void enqueue(const T &element) {
-    if (size == Capacity) {
-      std::cerr << "Queue is full. Unable to enqueue." << std::endl;
-      return;
+    ~Queue() {
+        delete[] elements;
     }
 
-    elements[rear] = element;
-    rear = (rear + 1) % Capacity;
-    ++size;
-  }
+    void enqueue(const T &element) {
+        if (size == capacity) {
+            resize();
+        }
 
-  T dequeue() {
-    if (empty()) {
-      std::cerr << "Queue is empty. Unable to dequeue." << std::endl;
-      throw std::out_of_range("Queue is empty");
+        elements[rear] = element;
+        rear = (rear + 1) % capacity;
+        ++size;
     }
 
-    T frontElement = elements[front];
-    front = (front + 1) % Capacity;
-    --size;
-    return frontElement;
-  }
+    T dequeue() {
+        if (empty()) {
+            std::cerr << "Queue is empty. Unable to dequeue." << std::endl;
+            throw std::out_of_range("Queue is empty");
+        }
 
-  T frontElement() const {
-    if (empty()) {
-      std::cerr << "Queue is empty." << std::endl;
-      throw std::out_of_range("Queue is empty");
+        T frontElement = elements[front];
+        front = (front + 1) % capacity;
+        --size;
+        return frontElement;
     }
 
-    return elements[front];
-  }
+    T frontElement() const {
+        if (empty()) {
+            std::cerr << "Queue is empty." << std::endl;
+            throw std::out_of_range("Queue is empty");
+        }
 
-  bool empty() const { return size == 0; }
+        return elements[front];
+    }
 
-  size_t queueSize() const { return size; }
+    bool empty() const { return size == 0; }
+
+    size_t queueSize() const { return size; }
 };
 
 #endif // QUEUE_H
+
--- main.cpp
+++ main.cpp
@@ -1,67 +1,12 @@
 // #include<bits/stdc++.h>
-#include "LinkedList.h"
-#include <algorithm>
-#include <ctime>
 #include <iostream>
-#include <vector>
-class Dog {
-private:
-    std::string name;
-    int age;
+#include "string"
+#include "sub-list.h"
 
-public:
-    // Constructor
-    Dog(std::string n, int a) {
-      name=n;
-      age=a;
-    }
-    Dog(): name("anon"), age(20){}
-    // Member function to bark
-    void bark() {
-        std::cout << name << " says Woof!" << std::endl;
-    }
-   void setName(string n)
-   {
-     name=n;
-   }
-   string getName()
-   {
-     return name;
-   }
-    // Member function to celebrate birthday
-    void celebrateBirthday() {
-        age++;
-        std::cout << "Happy Birthday, " << name << "! Now " << name << " is " << age << " years old." << std::endl;
-    }
-    
-    void setAge(int age)
-    {
-      this->age=age;
-    }
-    // Getter function to retrieve the age
-    int getAge() const {
-        return age;
-    }
-};
-void fun(string balh)
-{
-  cout<<balh<<endl;
-}
 int main() {
-    // Creating instances of the Dog class
-    Dog dog1("Buddy", 2);
-    fun("mohamed");
-    // Accessing attributes
-    std::cout << dog1.getAge() << std::endl;  
-    // Calling methods
-    dog1.bark();
-    int x=dog1.getAge();
-    cout<<dog1.getAge();
-    // Celebrating birthdays
-    dog1.celebrateBirthday();
-    dog1.setAge(4);
-    // Checking the updated age
-    std::cout << "Now, " << dog1.getAge() << " years old." << std::endl;
+    SubList<int>test;
+
+        
 
     return 0;
 }
