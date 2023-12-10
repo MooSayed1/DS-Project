1c5af6213c90a2fdb7824f921a61b79d634195fe
AnonMiraj <nabilmalek48@gmail.com>
2023-12-10 21:01:19 +0000

    added display all and fixed some bugs

--- CakeDisplay.cpp
+++ CakeDisplay.cpp
@@ -2,7 +2,9 @@
 // CakeDisplay.cpp
 #include "CakeDisplay.h"
 #include "Cake.h"
+#include <raylib.h>
 
+        cakeMap mapOfMaps;
 void simulateLoading() {
 
   size_t screenWidth = GetScreenWidth();
@@ -32,7 +34,7 @@ void simulateLoading() {
 
     // Draw loading text with larger font
     DrawText(TextFormat("%d%%", loadingPercentage), screenWidth / 2 - 30,
-             screenHeight / 2 - 30, 30, BLACK);
+             screenHeight / 2 - 50, 30, BLACK);
 
     EndDrawing();
 
@@ -97,53 +99,84 @@ void simulateLoading() {
 // }
 
 
-void displayOrders(const std::vector<Cake> &orders) {
-    size_t screenWidth = GetScreenWidth();
-    size_t screenHeight = GetScreenHeight();
 
-    SetTargetFPS(60);
+
+
+class CakeTable {
+public:
+    CakeTable(int screenWidth, int screenHeight, const char * windowTitle)
+        : screenWidth(screenWidth), screenHeight(screenHeight) {
+    }
+
+    ~CakeTable() {
+        CloseWindow();
+    }
+
+    void addCake(int id, int shape, int flavor, int glazing, int sprinkles) {
+        Cake cake( id, shape, flavor, glazing, sprinkles );
+        cakes.push_back(cake);
+    }
+
+void drawTable() {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    // Draw cakes
+    for (size_t i = 0; i < cakes.size(); i++) {
+        DrawText(TextFormat("%d", cakes[i].getId()), (screenWidth / 7.f), 40 + i * 30 - scrollY, 20, RED);
+        DrawText(mapOfMaps.shapeMap[cakes[i].getShape()].c_str(), 2 * (screenWidth / 7.f), 40 + i * 30 - scrollY, 20, GREEN);
+        DrawText(mapOfMaps.shapeMap[cakes[i].getFlavor()].c_str(), 3 * (screenWidth / 7.f), 40 + i * 30 - scrollY, 20, BLUE);
+        DrawText(mapOfMaps.shapeMap[cakes[i].getGlazing()].c_str(), 4 * (screenWidth / 7.f), 40 + i * 30 - scrollY, 20, PURPLE);
+        DrawText(mapOfMaps.shapeMap[cakes[i].getSprinkles()].c_str(), 5 * (screenWidth / 7.f), 40 + i * 30 - scrollY, 20, ORANGE);
+    }
+    DrawRectangle(0, 0, screenWidth, 30, RAYWHITE);
+    // Draw table headers
+    DrawText("ID", screenWidth / 7.f, 10, 20, RED);
+    DrawText("Shape", 2 * (screenWidth / 7.f), 10, 20, GREEN);
+    DrawText("Flavor", 3 * (screenWidth / 7.f), 10, 20, BLUE);
+    DrawText("Glazing", 4 * (screenWidth / 7.f), 10, 20, PURPLE);
+    DrawText("Sprinkles", 5 * (screenWidth / 7.f), 10, 20, ORANGE);
+
+
+    EndDrawing();
+
+    // Handle scrolling
+    float totalContentHeight = static_cast<float>(cakes.size() * 35);
+
+    if ((IsKeyDown(KEY_DOWN)||IsKeyDown(KEY_K))&& scrollY <= totalContentHeight - screenHeight) {
+        scrollY += 10.f;
+    }
+    if ((IsKeyDown(KEY_UP)||IsKeyDown(KEY_J)) && scrollY - 10 >= 0) {
+        scrollY -= 10.f;
+    }
+}
+
+
+private:
+    int screenWidth;
+    int screenHeight;
+    std::vector<Cake> cakes;
+    int scrollY = 0;
+};
+
+
+void displayOrders(const std::vector<Cake> &orders) {
+    const int screenWidth = 1366;
+    const int screenHeight = 768;
+
+    CakeTable cakeTable(screenWidth, screenHeight, "Cake Table");
+    for (auto & order:orders) {
+    cakeTable.addCake( order.getShape(), order.getFlavor(), order.getGlazing(), order.getSprinkles(),order.getId());
+    }
 
     while (!WindowShouldClose()) {
-        // Update
-
-        // Draw
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Cake Orders:", 20, 20, 20, DARKGRAY);
-
-        // Create a rectangle for all orders
-        Rectangle ordersRect = {20, 60, 500, static_cast<float>(30 * orders.size())};
-
-        // Draw a filled rectangle as the background for all orders
-        DrawRectangleRec(ordersRect, BLUE);
-
-        // Move Y position for the first order
-        int yPos = static_cast<int>(ordersRect.y + 5);
-
-        cakeMap mapOfMaps;
-
-        for (const auto &cake : orders) {
-            // Draw order details with larger font and white color
-            string orderDetails =
-                "ID: " + to_string(cake.getId()) +
-                " Shape: " + mapOfMaps.shapeMap[cake.getShape()] +
-                " Glazing: " + mapOfMaps.glazingMap[cake.getGlazing()] +
-                " Flavor: " + mapOfMaps.flavorMap[cake.getFlavor()] +
-                " Sprinkles: " + mapOfMaps.sprinklesMap[cake.getSprinkles()];
-
-            DrawText(orderDetails.c_str(), ordersRect.x + 10, yPos, 20, WHITE);
-
-            // Move Y position for the next order
-            yPos += 30; // Increase Y position and add some space between orders
-        }
-
-        EndDrawing();
-
-        // Return control to the main loop
+        cakeTable.drawTable();
         if (IsKeyPressed(KEY_ENTER)) {
             break;
         }
     }
+
+
+
+
 }
